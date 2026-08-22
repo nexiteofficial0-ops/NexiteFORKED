cc2174016a8a3b396b6544347b2eda04c8cb161c
spaceyy.win <kaydenblex707@gmail.com>
2026-08-21 19:24:06 -0700

    Use bundled Ultraviolet Wisp server

--- Ultraviolet-App-main/index.js
+++ Ultraviolet-App-main/index.js
@@ -145,7 +145,7 @@ async function navigate(input) {
 	updateNavigationButtons();
 	try {
 		await registerSW();
-		const wispUrl = "wss://www.cherri-education.online/socket/";
+		const wispUrl = `${location.protocol === "https:" ? "wss" : "ws"}://${location.host}/wisp/`;
 		const transportReady = (async () => {
 			if ((await connection.getTransport()) !== "/epoxy/index.mjs") {
 				await connection.setTransport("/epoxy/index.mjs", [{ wisp: wispUrl }]);
